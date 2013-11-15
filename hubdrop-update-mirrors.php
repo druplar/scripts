e757166dd02cbf369f653fd5b545caf289633e68
Jon Pugh <jon@thinkdrop.net>
2013-11-15 13:13:25 -0500

    removing config

--- hubdrop-update-mirrors.php
+++ hubdrop-update-mirrors.php
@@ -15,12 +15,6 @@ if ($handle = opendir($directory)) {
       print "Updating mirror in $full_path... \n";
 
       chdir($full_path);
-
-      // @TODO: Only need to leave this in for one jenkins round.
-      // all new repos will get this config as they are created.
-      exec('git config --local remote.origin.fetch "+refs/tags/*:refs/tags/*"');
-      exec('git config --local remote.origin.fetch "+refs/heads/*:refs/heads/*" --add');
-
       print hexec("git fetch -p origin");
       print hexec("git push --mirror");
     }
